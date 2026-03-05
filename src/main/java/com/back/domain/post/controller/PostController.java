eff707eaa21ce0bb36c51261c7df11729da0acb6
chacha86 <taejincha0619@gmail.com>
2026-03-05 14:51:06 +0900

    10

--- src/main/java/com/back/domain/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/controller/PostController.java
@@ -2,6 +2,7 @@ package com.back.domain.post.controller;
 
 import com.back.domain.post.entity.Post;
 import com.back.domain.post.service.PostService;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
@@ -25,18 +26,21 @@ public class PostController {
         return getWriteForm("", "", "", "");
     }
 
+    public static class WriteRequestForm {
+        @Size(min=2, max=10)
+        @NotBlank
+        private String title;
+
+        @NotBlank
+        @Size(min=2, max=100)
+        private String content;
+    }
+
     @PostMapping("/posts/write")
     @ResponseBody
-    public String write(
-            @Size(min=2, max=10)
-            @NotBlank
-            String title,
+    public String write(@Valid WriteRequestForm form) {
 
-            @NotBlank
-            @Size(min=2, max=100)
-            String content) {
-
-        Post post = postService.write(title, content);
+        Post post = postService.write(form.title, form.content);
 
         return "%d번 글이 작성되었습니다.".formatted(post.getId());
     }
