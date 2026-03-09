c2ddc5f32e5b73d9dad4be2430730c15577eb79d
chacha86 <taejincha0619@gmail.com>
2026-03-09 11:50:44 +0900

    38-1

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,10 +1,14 @@
-package com.back.domain.post.entity;
+package com.back.domain.post.post.entity;
 
+import com.back.domain.post.comment.entity.Comment;
 import com.back.global.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -14,6 +18,9 @@ public class Post extends BaseEntity {
 
     private String title;
     private String content;
+
+    @OneToMany
+    private List<Comment> comments;
 
     public void update(String title, String content) {
         this.title = title;
